19977dec6068949e947e3d4765f27fa622fdb354
Tyncha <59379795+tyncha@users.noreply.github.com>
2021-03-04 22:01:44 -0600

    Update output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "cluster_id" {
+    value = "${module.my-cluster.cluster_id}"
+}
+output "cluster_arn" {
+    value = "${module.my-cluster.cluster_arn}"
+}
+output "cluster_version" {
+    value = "${module.my-cluster.cluster_version}"
+}
