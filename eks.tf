2a0e9ac5af8bf299d0f8355001d4e580c9b9cb6f
Tyncha <59379795+tyncha@users.noreply.github.com>
2021-03-04 22:01:18 -0600

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "cluster" {
-  name = module.my_cluster.cluster_id 
+  name = module.my-cluster.cluster_id 
   }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.my_cluster.cluster_id
+  name = module.my-cluster.cluster_id
 }
 
 provider "kubernetes" {
@@ -13,19 +13,18 @@ provider "kubernetes" {
   load_config_file       = false
   version                = "~> 1.9"
 }
-module "my-clsuter" {
-    source = "terraform-aws-module/eks/aws"
-    version = "12.0.0"
-    cluster_name = "my-cluster"
-    cluster_version = "1.14"
-    subnets = ["subnet-0b73d0ecff5035baf", "subnet-02e78872a7c64aa30", "subnet-0833b17fce00b87d3"]
-    vpc_id =  "vpc-0626f00e3d58f59c1"
-    workers_group = [
-    {
-        instance_type = "t2.small"
-        asg_max_size = 3
-        asg_min_size = 2
+
+module "my-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "12.0.0"
+  cluster_version = "${var.cluster_version}"
+  cluster_name    = "${var.cluster_name}"
+  subnets         = "${var.subnets}"
+  vpc_id          = "${var.vpc_id}"
+  worker_groups = [{
+      instance_type = "${var.instance_type}"
+      asg_max_size  = "${var.asg_max_size}"
+      asg_min_size = "${var.asg_min_size}"
     }
-    ]
-    
+  ]
 }
